3f2a558e69391e6f69b4926f62e0eea36279e03d
Jean Joeris <joeris.peter@gmail.com>
2016-09-10 11:21:11 -0600

    added model testing for locations

---
+++ spec/models/location_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Location, type: :model do
+  context "given a name" do
+    it "has the name attribute" do
+      location = Location.new(name: "Big workspace bathrooms")
+
+      expect(location).to be_valid
+      expect(location.name).to eq "Big workspace bathrooms"
+    end
+  end
+
+  context "given no name" do
+    it "requires a name" do
+      location = Location.new
+
+      expect(location).to be_invalid
+    end
+  end
+end
