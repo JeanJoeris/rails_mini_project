e808ea83efc388d78f2f48decd38beb273cde8f6
Jean Joeris <joeris.peter@gmail.com>
2016-09-10 11:54:38 -0600

    added belongs to user relation on sightings

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -1,6 +1,7 @@
 class Sighting < ApplicationRecord
   belongs_to :animal
   belongs_to :location
+  belongs_to :user
 
   validates :sighting_time, presence: true
 end
--- spec/models/sighting_spec.rb
+++ spec/models/sighting_spec.rb
@@ -3,17 +3,20 @@ require 'rails_helper'
 RSpec.describe Sighting, type: :model do
   it { should belong_to(:location) }
   it { should belong_to(:animal) }
+  it { should belong_to(:user) }
 
   context "given the right attributes" do
     it "has the attributes accessible" do
       animal = create(:animal)
       location = create(:location)
+      user = create(:user)
 
-      sighting = Sighting.new(animal: animal, location: location, sighting_time: DateTime.now)
+      sighting = user.sightings.new(animal: animal, location: location, sighting_time: DateTime.now)
 
       expect(sighting).to be_valid
       expect(sighting.animal).to eq(animal)
       expect(sighting.location).to eq(location)
+      expect(sighting.user).to eq(user)
     end
   end
 
