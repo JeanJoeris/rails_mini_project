db3ed4f0889928d423c7bd6a4a6de71ba2931a48
Jean Joeris <joeris.peter@gmail.com>
2016-09-11 10:33:59 -0600

    have basic function for grabbing taxonomical data from wikipedia with tests

--- spec/models/wiki_parser_spec.rb
+++ spec/models/wiki_parser_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe WikiParser, type: :model do
-  it "grabs the taxonomical levels of a real species" do
-    wp = WikiParser.new
-    taxonomical_data = wp.get_taxonomical_data("Platypus")
+  it "grabs the taxonomical data of a real species" do
+    taxonomical_data = WikiParser.get_taxonomical_data("Platypus")
 
     expect(taxonomical_data["Kingdom"]).to eq("Animalia")
     expect(taxonomical_data["Phylum"]).to eq("Chordata")
@@ -11,6 +10,24 @@ RSpec.describe WikiParser, type: :model do
     expect(taxonomical_data["Order"]).to eq("Monotremata")
     expect(taxonomical_data["Family"]).to eq("Ornithorhynchidae")
     expect(taxonomical_data["Genus"]).to eq("Ornithorhynchus")
-    expect(taxonomical_data["Species"]).to eq("anatinus")
+    expect(taxonomical_data["Species"]).to eq("Anatinus")
+  end
+
+  it "grabs the taxonomical data of a real genus" do
+    taxonomical_data = WikiParser.get_taxonomical_data("Zebra")
+
+    expect(taxonomical_data["Kingdom"]).to eq("Animalia")
+    expect(taxonomical_data["Phylum"]).to eq("Chordata")
+    # expect(taxonomical_data["Class"]).to eq("Mammalia")
+    expect(taxonomical_data["Order"]).to eq("Perissodactyla")
+    expect(taxonomical_data["Family"]).to eq("Equidae")
+    expect(taxonomical_data["Genus"]).to eq("Equus")
+    # expect(taxonomical_data["Species"]).to eq("Anatinus")
+  end
+
+  it "grabs nothing for non-animals" do
+    taxonomical_data = WikiParser.get_taxonomical_data("Car")
+
+    expect(taxonomical_data.values.uniq).to eq([nil])
   end
 end
