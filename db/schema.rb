4befbe6f743f6c003eafa60db2fa6230636a31d3
Jean Joeris <joeris.peter@gmail.com>
2016-09-11 11:17:48 -0600

    added migration and model for species

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160911171524) do
+ActiveRecord::Schema.define(version: 20160911171655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,12 @@ ActiveRecord::Schema.define(version: 20160911171524) do
     t.index ["animal_id"], name: "index_sightings_on_animal_id", using: :btree
     t.index ["location_id"], name: "index_sightings_on_location_id", using: :btree
     t.index ["user_id"], name: "index_sightings_on_user_id", using: :btree
+  end
+
+  create_table "species", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "taxonomical_classes", force: :cascade do |t|
