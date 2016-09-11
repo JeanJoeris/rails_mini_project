da9ee49a9b7fb363e0987fbf003acb84da8031b0
Jean Joeris <joeris.peter@gmail.com>
2016-09-11 10:41:58 -0600

    added migration and basic model class for kingdom

---
+++ app/models/kingdom.rb
@@ -0,0 +1,3 @@
+class Kingdom < ApplicationRecord
+
+end
---
+++ db/migrate/20160911164035_create_kingdom.rb
@@ -0,0 +1,8 @@
+class CreateKingdom < ActiveRecord::Migration[5.0]
+  def change
+    create_table :kingdoms do |t|
+      t.string :name
+      t.timestamps null: false
+    end
+  end
+end
