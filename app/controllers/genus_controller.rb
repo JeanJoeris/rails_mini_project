ded638b403a8ab70a188ef6051bfa30a4301b92e
Jean Joeris <joeris.peter@gmail.com>
2016-09-12 07:46:30 -0600

    added index for all taxonomical levels using a partial

--- app/controllers/genus_controller.rb
+++ app/controllers/genus_controller.rb
@@ -2,4 +2,8 @@ class GenusController < ApplicationController
   def show
     @genus = Genus.find(params[:id])
   end
+
+  def index
+    @genus = Genus.all
+  end
 end
--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -2,4 +2,8 @@ class SpeciesController < ApplicationController
   def show
     @species = Species.find(params[:id])
   end
+
+  def index
+    @species = Species.all
+  end
 end
